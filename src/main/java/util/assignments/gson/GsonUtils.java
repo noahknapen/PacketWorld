a36ae5dfca0f16a20a6219dbdcad316a5dddc146
CharlesVancoppenolle <charles.vancoppenolle@student.uhasselt.be>
2022-04-26 21:31:06 +0200

    Refactoring from scratch (temporary 12 charging stations not working)

--- src/main/java/util/assignments/gson/GsonUtils.java
+++ src/main/java/util/assignments/gson/GsonUtils.java
@@ -9,6 +9,7 @@ import util.assignments.targets.ChargingStation;
 import util.assignments.targets.Destination;
 import util.assignments.targets.Packet;
 import util.assignments.targets.Target;
+import util.assignments.task.Task;
 
 /**
  * A class that implements functions regarding the Gson library
@@ -27,13 +28,13 @@ public class GsonUtils {
         // Parametrize the Gson builder
         gsonBuilder.enableComplexMapKeySerialization();
 
+        gsonBuilder.registerTypeAdapter(Optional.class, new OptionalDeserializer<>());
+
         RuntimeTypeAdapterFactory<Target> factory = RuntimeTypeAdapterFactory.of(Target.class, "type")
             .registerSubtype(Packet.class, "Packet")
             .registerSubtype(Destination.class, "Destination")
             .registerSubtype(ChargingStation.class, "ChargingStation");
         gsonBuilder.registerTypeAdapterFactory(factory);
-
-        gsonBuilder.registerTypeAdapter(Optional.class, new OptionalDeserializer<>());
 
         // Create the Gson object and return it
         return gsonBuilder.create();   
