5e5086b5d2839d2e49930395f9be1d7790547cf9
CharlesVancoppenolle <charles.vancoppenolle@student.uhasselt.be>
2022-05-03 20:08:07 +0200

    Added the update of the charging station that is currently in use by an agent

--- src/main/java/agent/behavior/assignment_2/behaviors/ChargingBehavior.java
+++ src/main/java/agent/behavior/assignment_2/behaviors/ChargingBehavior.java
@@ -4,8 +4,18 @@ import agent.AgentAction;
 import agent.AgentCommunication;
 import agent.AgentState;
 import agent.behavior.Behavior;
+import environment.Coordinate;
 import util.assignments.general.GeneralUtils;
+import util.assignments.memory.MemoryKeys;
+import util.assignments.memory.MemoryUtils;
+import util.assignments.targets.ChargingStation;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 public class ChargingBehavior extends Behavior {
 
@@ -25,6 +35,40 @@ public class ChargingBehavior extends Behavior {
 
     @Override
     public void act(AgentState agentState, AgentAction agentAction) {
+        try {
+            updateCharginStation(agentState);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
         agentAction.skip();
     }
+
+    /////////////
+    // METHODS //
+    /////////////
+    
+    /**
+     * Update the state of the chargingStation that is currently in use by the agent
+     * 
+     * @throws IOException
+     * @throws JsonMappingException
+     * @throws JsonParseException
+     */
+    private void updateCharginStation(AgentState agentState) throws JsonParseException, JsonMappingException, IOException {
+        // Get charging station position
+        Coordinate charginStationCoordinate = new Coordinate(agentState.getX(), agentState.getY() - 1);
+        
+        // Get the current charging stations
+        ArrayList<ChargingStation> chargingStations = MemoryUtils.getListFromMemory(agentState, MemoryKeys.DISCOVERED_CHARGING_STATIONS, ChargingStation.class);
+
+        for(ChargingStation chargingStation: chargingStations) {
+            if(chargingStation.getCoordinate().equals(charginStationCoordinate)) {
+                chargingStation.setBatteryOfUser(Optional.of(agentState.getBatteryState()));
+                chargingStation.setInUse(true);
+                return;
+            }
+        }
+    } 
 }
