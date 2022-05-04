6f5d5a6fa78353e15454bd8ec7cad3e92c4ecf9d
Vince Marien <vince.marien@student.kuleuven.be>
2022-05-05 00:06:50 +0200

    removed some redundant print statements

--- src/main/java/agent/behavior/assignment_2/behaviors/MoveRandomlyBehavior.java
+++ src/main/java/agent/behavior/assignment_2/behaviors/MoveRandomlyBehavior.java
@@ -15,10 +15,9 @@ public class MoveRandomlyBehavior extends Behavior {
 
     @Override
     public void communicate(AgentState agentState, AgentCommunication agentCommunication) {
-        System.out.println("Comm1");
         // Communicate the charging stations with all the other agents
         GeneralUtils.handleChargingStations(agentState, agentCommunication);
-        System.out.println("Comm2");
+
         // Communicate the destination locations with agents in perception
         GeneralUtils.handleDestinationLocations(agentState, agentCommunication);
 
@@ -26,14 +25,11 @@ public class MoveRandomlyBehavior extends Behavior {
 
     @Override
     public void act(AgentState agentState, AgentAction agentAction) {
-        System.out.println("act1");
         // Check the perception of the agent
         GeneralUtils.checkPerception(agentState);
-        System.out.println("act2");
 
         // Build the graph
         GraphUtils.build(agentState);
-        System.out.println("act3");
 
         // Move the agent randomly
         ActionUtils.moveRandomly(agentState, agentAction);
