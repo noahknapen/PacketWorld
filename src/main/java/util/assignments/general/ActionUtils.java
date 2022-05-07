6bde7b22b51d11964167774348ba9866516b333a
Ludvig Fors <ludfo119@student.liu.se>
2022-05-07 10:35:53 +0200

    Added some docmentation and some updates

--- src/main/java/util/assignments/general/ActionUtils.java
+++ src/main/java/util/assignments/general/ActionUtils.java
@@ -87,11 +87,12 @@ public class ActionUtils {
     public static void moveToPosition(AgentState agentState, AgentAction agentAction, Coordinate coordinate) {
 
         // Check if the position is in the perception of the agent
-        /*if(GeneralUtils.positionInPerception(agentState, coordinate)) {
+
+        /*
+        if(GeneralUtils.positionInPerception(agentState, coordinate)) {
             Coordinate move = calculateMoveDefault(agentState, coordinate);
             makeMove(agentState, agentAction, move);
         }
-
          */
 
         // Check if the position is in the graph
